df8a391f4437b97847904edf0e7ea5dfa50a4b16
bosswanttolearn <68011820@kmitl.ac.th>
2026-02-16 10:18:05 +0700

    04

--- src/practicals/p03/product.ts
+++ src/practicals/p03/product.ts
@@ -1,2 +1,19 @@
-export class Product {}
+export class Product {
+  public readonly name: string
+  protected price: number = 0
 
+  static readonly DISCOUNT_PERCENT: number = 10
+
+  constructor(name: string = '', price: number = 0) {
+    this.name = name
+    this.price = price
+  }
+
+  getPrice(): number {
+    return this.price
+  }
+
+  setPrice(price: number): void {
+    this.price = price
+  }
+}
