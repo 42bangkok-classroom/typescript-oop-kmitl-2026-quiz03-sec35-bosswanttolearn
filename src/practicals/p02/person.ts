4082fb9047b2ca444c7ca127eb719acec0c0a42d
bosswanttolearn <68011820@kmitl.ac.th>
2026-02-16 10:03:16 +0700

    02

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -1,1 +1,18 @@
-export class Person {}
+export class Person {
+    
+    firstname: string = ''
+    lastname: string = ''
+
+    private age: number = 0
+    static country: string = 'Thailand '
+
+    set(age : number): void {
+        this.age = age
+    }
+    get age(): number {
+        return this.age
+    }
+    getFullName(): string {
+        return `${this.firstname} ${this.lastname}`
+    }
+}
